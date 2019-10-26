71714482f111cc409f4c57ca2f52d4721c0317f3
Andrew Capp <acapp909@gmail.com>
2019-10-26 19:33:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog
+  @@all = []
+  @@dog_names = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+
+  def self.all
+    @@all
+
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    @@dog_names.uniq.each {|element| puts element}
+  end
+
+  def save
+    @@all << self
+    @@dog_names << name
+  end
+
+end
